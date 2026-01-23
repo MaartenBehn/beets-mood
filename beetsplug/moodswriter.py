0f143a8835c3644b12d93329a620a6228d3b9b3d
Maarten Behn <maarten.behn@gmail.com>
2026-01-24 00:02:19 +0100

    fix

--- beetsplug/moodswriter.py
+++ beetsplug/moodswriter.py
@@ -25,31 +25,31 @@ class MoodsWriterPlugin(BeetsPlugin):
     factor = 0.0 
     min_mood = 0.5
 
-    if item['mood_acoustic'] is not None and item['mood_acoustic'] > min_mood:
+    if 'mood_acoustic' in item and item['mood_acoustic'] is not None and item['mood_acoustic'] > min_mood:
         mood = "acoustic"
         factor = item['mood_acoustic']
 
-    if item['mood_aggressive'] is not None and item['mood_aggressive'] > min_mood and item['mood_aggressive'] > factor:
+    if 'mood_aggressive' in item and item['mood_aggressive'] is not None and item['mood_aggressive'] > min_mood and item['mood_aggressive'] > factor:
         mood = "aggressive"
         factor = item['mood_aggressive']
 
-    if item['mood_electronic'] is not None and item['mood_electronic'] > min_mood and item['mood_electronic'] > factor:
+    if 'mood_electronic' in item and item['mood_electronic'] is not None and item['mood_electronic'] > min_mood and item['mood_electronic'] > factor:
         mood = "electronic"
         factor = item['mood_electronic']
 
-    if item['mood_happy'] is not None and item['mood_happy'] > min_mood and item['mood_happy'] > factor:
+    if 'mood_happy' in item and item['mood_happy'] is not None and item['mood_happy'] > min_mood and item['mood_happy'] > factor:
         mood = "happy"
         factor = item['mood_happy']
 
-    if item['mood_party'] is not None and item['mood_party'] > min_mood and item['mood_party'] > factor:
+    if 'mood_party' in item and item['mood_party'] is not None and item['mood_party'] > min_mood and item['mood_party'] > factor:
         mood = "party"
         factor = item['mood_party']
 
-    if item['mood_relaxed'] is not None and item['mood_relaxed'] > min_mood and item['mood_relaxed'] > factor:
+    if 'mood_relaxed' in item and item['mood_relaxed'] is not None and item['mood_relaxed'] > min_mood and item['mood_relaxed'] > factor:
         mood = "relaxed"
         factor = item['mood_relaxed']
 
-    if item['mood_sad'] is not None and item['mood_sad'] > min_mood and item['mood_sad'] > factor:
+    if 'mood_sad' in item and item['mood_sad'] is not None and item['mood_sad'] > min_mood and item['mood_sad'] > factor:
         mood = "sad"
 
     return mood
